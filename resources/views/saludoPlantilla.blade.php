efc0d477d2db6e1c3168d8bee26b35a5ee121867
SeiKo093 <jcruz_20@alu.uabcs.mx>
2023-11-16 00:54:09 -0600

    dev3 base

---
+++ resources/views/saludoPlantilla.blade.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>PlantillaBlade_saludo</title>
+</head>
+
+<body>
+
+    <h3> Te mando un saludote O.O <?= $nom ?></h3>
+
+</body>
+
+</html>
