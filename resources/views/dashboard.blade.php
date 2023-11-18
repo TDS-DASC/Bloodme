fd683e5cef1d952341c019a8ab5a5278d4944831
jonathanj20 <jonathanpossada@gmail.com>
2023-11-17 17:14:52 -0700

    Frontend - Modulo: Dashboard

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -12,6 +12,9 @@
     <!-- BOOSTRAP -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0/dist/css/bootstrap.min.css" rel="stylesheet">
 
+    <!--fontawesome-->
+    <script src="https://kit.fontawesome.com/675de417d6.js" crossorigin="anonymous"></script>
+
 
     <style>
         header {
@@ -52,7 +55,7 @@
                                 <img src="{{asset('images/logo.png')}}" alt="" width="70px">
                             </span>
                         </a>
-                        <h2 style="display: inline-block">BLOODME</h2>
+                        <h2 style="display: inline-block" class="text-danger">BLOODME</h2>
                     </div>
                 </div>
 
@@ -78,7 +81,7 @@
 
      <!-- ========== NAVBAR ========== -->
     <nav class="navbar navbar-expand-lg navbar-light bg-light border-top border-bottom py-1">
-    <div class="container">
+    <div class="container-fluid ms-5">
 
         <div id="scrollbar" class="d-flex justify-content-start align-items-center">
             <ul class="navbar-nav">
@@ -123,150 +126,101 @@
 <div class="main-content ml-5 mt-5">
 
     <div class="page-content">
-        <div class="container-fluid">
+        <div class="container">
 
-            <div class="row">
-                <div class="col-xxl-5">
-                    <div class="d-flex flex-column h-100">
-                        
-                        <div class="row">
-                            <div class="col-md-6">
-                                <div class="card card-animate">
-                                    <div class="card-body">
-                                        <div class="d-flex justify-content-between">
-
-
-                                            <div>
-                                                <p class="fw-medium text-muted mb-0">Campañas</p>
-                                                <h2 class="mt-4 ff-secondary fw-semibold"><span class="counter-value" data-target="28.05">0</span>k</h2>
-                                               
-                                            </div>
-                                            
-                                        </div>
-                                    </div><!-- end card body -->
-                                </div> <!-- end card-->
-                            </div> <!-- end col-->
-
-                            <div class="col-md-6">
-                                <div class="card card-animate">
-                                    <div class="card-body">
-                                        <div class="d-flex justify-content-between">
-
-
-                                            <div>
-                                                <p class="fw-medium text-muted mb-0">Solicitudes</p>
-                                                <h2 class="mt-4 ff-secondary fw-semibold"><span class="counter-value" data-target="28.05">0</span>k</h2>
-                                               
-                                            </div>
-                                            
-                                        </div>
-                                    </div><!-- end card body -->
-                                </div> <!-- end card-->
-                            </div> <!-- end col-->
-                        </div> <!-- end row-->
-
-                        <div class="row">
-                            <div class="col-md-6">
-                                <div class="card card-animate">
-                                    <div class="card-body">
-                                        <div class="d-flex justify-content-between">
-
-
-                                            <div>
-                                                <p class="fw-medium text-muted mb-0">Donaciones</p>
-                                                <h2 class="mt-4 ff-secondary fw-semibold"><span class="counter-value" data-target="28.05">0</span>k</h2>
-                                               
-                                            </div>
-                                            
-                                        </div>
-                                    </div><!-- end card body -->
-                                </div> <!-- end card-->
-                            </div> <!-- end col-->
-
-                            <div class="col-md-6">
-                                <div class="card card-animate">
-                                    <div class="card-body">
-                                        <div class="d-flex justify-content-between">
-
-
-                                            <div>
-                                                <p class="fw-medium text-muted mb-0">???</p>
-                                                <h2 class="mt-4 ff-secondary fw-semibold"><span class="counter-value" data-target="28.05">0</span>k</h2>
-                                               
-                                            </div>
-                                            
-                                        </div>
-                                    </div><!-- end card body -->
-                                </div> <!-- end card-->
-                            </div> <!-- end col-->
-                        </div> <!-- end row-->
+            <div class="row w-100 d-flex justify-content-center">
+                <div class="col-3 ">
+                    <div class="card card-animate">
+                        <div class="card-body">
+                                <div>
+                                    <p class="fw-medium text-muted mb-0">Campañas</p>
+                                    <h2 class="mt-4 ff-secondary fw-semibold"><span class="counter-value" data-target="28.05"><i class="fa-solid fa-tents"></i> 0</span></h2>
+                                </div>
+                        </div><!-- end card body -->
                     </div>
-                </div> <!-- end col-->
+                </div>
+                <div class="col-3">
+                    <div class="card card-animate">
+                        <div class="card-body">
+                            <div class="d-flex justify-content-between">
+                                <div>
+                                    <p class="fw-medium text-muted mb-0">Solicitudes</p>
+                                    <h2 class="mt-4 ff-secondary fw-semibold"><span class="counter-value" data-target="28.05"><i class="fa-solid fa-envelope"></i> 0</span></h2>
+                                </div>
+                            </div>
+                        </div><!-- end card body -->
+                    </div> <!-- end card-->
+                </div>
+                <div class="col-3">
+                    <div class="card card-animate">
+                        <div class="card-body">
+                            <div class="d-flex justify-content-between">
+                                <div>
+                                    <p class="fw-medium text-muted mb-0">Donaciones</p>
+                                    <h2 class="mt-4 ff-secondary fw-semibold tex-center"><span class="counter-value" data-target="28.05"><i class="fa-solid fa-hand-holding-dollar"></i> 0</span></h2>
+                                   
+                                </div>
+                                
+                            </div>
+                        </div><!-- end card body -->
+                    </div> <!-- end card-->
+                </div>
+            </div>
+            <div class="row">
+                <div class="col-6">
+                    <script type="text/javascript" src="https://www.gstatic.com/charts/loader.js"></script>
+                    <script type="text/javascript">
+                        google.charts.load('current', {'packages':['corechart']});
+                        google.charts.setOnLoadCallback(drawChart);
 
-                <div class="col-xxl-7">
-                    <div class="row h-100">
-                        <div class="col-xl-6">
-                            <div class="card card-height-100">
-                                <div class="card-header align-items-center d-flex">
-                                    <h4 class="card-title mb-0 flex-grow-1">Aqui llenar con algo</h4>
-                                </div><!-- end card header -->
+                        function drawChart() {
 
-                                <!-- card body -->
-                                <div class="card-body">
+                            var data = google.visualization.arrayToDataTable([
+                                ['Task', 'Hours per Day'],
+                                ['Campañas',     11],
+                                ['Solicitudes',      2],
+                                ['Donaciones',  2],
+                                ['Usuarios', 10],
+                                ['Donadores',    7]
+                            ]);
 
-                                    <div id="users-by-country" data-colors='["--vz-light"]' class="text-center" style="height: 252px"></div>
+                            var options = {
+                                title: 'Datos generales'
+                            };
 
-                                    <div class="table-responsive table-card mt-3">
-                                        <table class="table table-borderless table-sm table-centered align-middle table-nowrap mb-1">
-                                            <thead class="text-muted border-dashed border border-start-0 border-end-0 bg-light-subtle">
-                                                <tr>
-                                                    <th style="width: 30%;">PARAMETRO 1</th>
-                                                    <th style="width: 30%;">PARAMETRO 2</th>
-                                                    <th style="width: 30%;">PARAMETRO 3</th>
-                                                </tr>
-                                            </thead>
-                                            <tbody class="border-0">
-                                                <tr>
-                                                    <td>0-30</td>
-                                                    <td>2,250</td>
-                                                    <td>4,250</td>
-                                                </tr>
-                                                <tr>
-                                                    <td>31-60</td>
-                                                    <td>1,501</td>
-                                                    <td>2,050</td>
-                                                </tr>
-                                                <tr>
-                                                    <td>61-120</td>
-                                                    <td>750</td>
-                                                    <td>1,600</td>
-                                                </tr>
-                                                <tr>
-                                                    <td>121-240</td>
-                                                    <td>540</td>
-                                                    <td>1,040</td>
-                                                </tr>
-                                            </tbody>
-                                        </table>
+                            var chart = new google.visualization.PieChart(document.getElementById('piechart'));
+
+                            chart.draw(data, options);
+                        }
+                    </script>
+                    <div id="piechart" style="width: 900px; height: 500px;"></div>
+                </div>
+                <div class="col-6 mt-5">
+                    <div class="col-12 w-50">
+                        <div class="card card-animate">
+                            <div class="card-body">
+                                    <div>
+                                        <p class="fw-medium text-muted mb-0">Usuarios</p>
+                                        <h2 class="mt-4 ff-secondary fw-semibold"><span class="counter-value" data-target="28.05"><i class="fa-solid fa-user"></i> 0</span></h2>
+                                    </div>
+                            </div><!-- end card body -->
+                        </div>
+                    </div>
+                    <div class="col-12 mt-5 w-50">
+                        <div class="card card-animate">
+                            <div class="card-body">
+                                <div class="d-flex justify-content-between">
+                                    <div>
+                                        <p class="fw-medium text-muted mb-0">Donadores</p>
+                                        <h2 class="mt-4 ff-secondary fw-semibold"><span class="counter-value" data-target="28.05"><i class="fa-solid fa-heart-circle-plus"></i> 0</span></h2>
                                     </div>
                                 </div>
-                                <!-- end card body -->
-                            </div><!-- end card -->
-                        </div><!-- end col -->
-
-                       
-
-                    </div> <!-- end row-->
-                </div><!-- end col -->
-            </div> <!-- end row-->
-
-          
-
-            <div class="row">
-                <div class="col-xl-4">
-                    <div class="card card-height-100">
+                            </div><!-- end card body -->
+                        </div> <!-- end card-->
+                    </div>
+                    {{-- <div class="card card-height-100">
                         <div class="card-header align-items-center d-flex">
-                            <h4 class="card-title mb-0 flex-grow-1">Usuarios Registrados?</h4>
+                            <h4 class="card-title mb-0 flex-grow-1">Usuarios Registrados</h4>
                             <div class="flex-shrink-0">
                                 <div class="dropdown card-header-dropdown">
                                     <a class="text-reset dropdown-btn" href="#" data-bs-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
@@ -279,7 +233,7 @@
                             <div id="user_device_pie_charts" data-colors='["--vz-primary", "--vz-warning", "--vz-info"]' class="apex-charts" dir="ltr"></div>
 
                             <div class="table-responsive mt-3">
-                                <table class="table table-borderless table-sm table-centered align-middle table-nowrap mb-0">
+                                <table class="table table-borderless table-sm table-centered align-middle table-nowrap mb-0 w-10">
                                     <tbody class="border-0">
                                         <tr>
                                             <td>
@@ -309,12 +263,9 @@
                                 </table>
                             </div>
                         </div><!-- end card body -->
-                    </div><!-- end card -->
+                    </div><!-- end card --> --}}
                 </div><!-- end col -->
-
-               
-
-               
+            </div>
         </div>
         <!-- container-fluid -->
     </div>
