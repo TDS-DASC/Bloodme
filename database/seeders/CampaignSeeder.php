22e0f4e05ebb917c52c0f5b85d03c43fcb9190ea
raulaloisy2 <raul_aloisy@live.com.mx>
2023-11-22 22:02:00 -0700

    Añadido seeders a campañas

--- database/seeders/CampaignSeeder.php
+++ database/seeders/CampaignSeeder.php
@@ -41,5 +41,50 @@ class CampaignSeeder extends Seeder
         $campaign->current_donations = 5;
         $campaign->user_id = 1;
         $campaign->save();
+
+        $campaign = new Campaign();
+        $campaign->start_campaign = "2022-12-10";
+        $campaign->platelets = 0;
+        $campaign->blood = 1;
+        $campaign->donations_required = 10;
+        $campaign->current_donations = 5;
+        $campaign->user_id = 1;
+        $campaign->save();
+
+        $campaign = new Campaign();
+        $campaign->start_campaign = "2022-12-15";
+        $campaign->platelets = 0;
+        $campaign->blood = 1;
+        $campaign->donations_required = 10;
+        $campaign->current_donations = 5;
+        $campaign->user_id = 1;
+        $campaign->save();
+
+        $campaign = new Campaign();
+        $campaign->start_campaign = "2022-12-20";
+        $campaign->platelets = 0;
+        $campaign->blood = 1;
+        $campaign->donations_required = 10;
+        $campaign->current_donations = 5;
+        $campaign->user_id = 1;
+        $campaign->save();
+
+        $campaign = new Campaign();
+        $campaign->start_campaign = "2022-12-25";
+        $campaign->platelets = 0;
+        $campaign->blood = 1;
+        $campaign->donations_required = 10;
+        $campaign->current_donations = 5;
+        $campaign->user_id = 1;
+        $campaign->save();
+
+        $campaign = new Campaign();
+        $campaign->start_campaign = "2022-12-31";
+        $campaign->platelets = 0;
+        $campaign->blood = 1;
+        $campaign->donations_required = 10;
+        $campaign->current_donations = 5;
+        $campaign->user_id = 1;
+        $campaign->save();
     }
 }
