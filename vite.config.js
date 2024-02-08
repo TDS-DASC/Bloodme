1f2c3e2696948585df4a04a097503c231db3a174
VictorMRD <116592163+VictorMRD@users.noreply.github.com>
2024-02-08 13:19:41 -0700

    Create project

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,20 @@
-import { defineConfig } from 'vite';
-import laravel from 'laravel-vite-plugin';
+import { defineConfig } from "vite";
+import path from "path";
+import Vue from "@vitejs/plugin-vue";
+import ViteImages from "vite-plugin-vue-images";
 
+// https://vitejs.dev/config/
 export default defineConfig({
-    plugins: [
-        laravel({
-            input: ['resources/css/app.css', 'resources/js/app.js'],
-            refresh: true,
-        }),
-    ],
+  plugins: [
+    Vue(),
+    ViteImages({
+      dirs: ["src/assets/images"],
+    }),
+  ],
+  resolve: {
+    extensions: [".mjs", ".js", ".ts", ".jsx", ".tsx", ".json", ".vue", ".css"],
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
+    },
+  },
 });
