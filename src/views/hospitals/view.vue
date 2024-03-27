369d83b2fd668f3430d7726072b3d66fde27ff1e
VictorMRD <victormrd2002@hotmail.com>
2024-03-27 16:50:29 -0700

    Add hospital view

--- src/views/hospitals/view.vue
+++ src/views/hospitals/view.vue
@@ -1,82 +1,21 @@
 <template>
     <div class="">
         <div class="space-y-5 profile-page">
-            <div class="profiel-wrap px-[35px] pb-10 md:pt-[84px] pt-10 rounded-lg bg-white dark:bg-slate-800 lg:flex lg:space-y-0 space-y-6 justify-between items-end relative z-[1]">
-                <div class="bg-slate-900 dark:bg-slate-700 absolute left-0 top-0 md:h-1/2 h-[150px] w-full z-[-1] rounded-t-lg"></div>    
-                    <div class="profile-box flex-none md:text-start text-center">
-                        <div class="md:flex items-end md:space-x-6 rtl:space-x-reverse">
-                            <div class="flex-none">
-                                <div class="md:h-[186px] md:w-[186px] h-[140px] w-[140px] md:ml-0 md:mr-0 ml-auto mr-auto md:mb-0 mb-4 rounded-full ring-4 ring-slate-100 relative">
-                                    <img :src="userData.image_url" alt="" class="w-full h-full object-cover rounded-full" v-if="userData" />
-                                    <img src="@/assets/images/avatar/av-1.svg" alt="" class="w-full h-full object-cover rounded-full" v-else />
-                                        <router-link
-                                            :to="`/hospitals/${id}/edit`"
-                                            class="absolute right-2 h-8 w-8 bg-slate-50 text-slate-600 rounded-full shadow-sm flex flex-col items-center justify-center md:top-[140px] top-[100px]"
-                                            ><Icon icon="heroicons:pencil-square" />
-                                        </router-link>
-                                </div>
-                            </div>
-                            <div class="flex-1">
-                                <div class="text-2xl font-medium text-slate-900 dark:text-slate-200 mb-[3px]" v-if="userData">
-                                    {{ userData.name }} {{ userData.lastname }}
-                                </div>
-                                <div class="text-2xl font-medium text-slate-900 dark:text-slate-200 mb-[3px]" v-else>
-                                    Cargando...
-                                </div>
-                                <div class="text-sm font-light text-slate-600 dark:text-slate-400">
-                                    Nombre completo
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                    <!-- end profile box -->
-                    <div    class="profile-info-500 md:flex md:text-start text-center flex-1 max-w-[516px] md:space-y-0 space-y-4">
-                        <div class="flex-1">
-                            <div class="text-base text-slate-900 dark:text-slate-300 font-medium mb-1">
-                                33
-                            </div>
-                            <div class="text-sm text-slate-600 font-light dark:text-slate-300">
-                                Donaciones totales
-                            </div>
-                        </div>
-                    <!-- end single -->
-                    <div class="flex-1">
-                        <div class="text-base text-slate-900 dark:text-slate-300 font-medium mb-1">
-                            2
-                        </div>
-                        <div class="text-sm text-slate-600 font-light dark:text-slate-300">
-                            Campañas creadas
-                        </div>
-                    </div>
-                    <!-- end single -->
-                    <div class="flex-1">
-                        <div class="text-base text-slate-900 dark:text-slate-300 font-medium mb-1">
-                            9 
-                        </div>
-                        <div class="text-sm text-slate-600 font-light dark:text-slate-300">
-                            Citas agendadas
-                        </div>
-                    </div>
-                    <!-- end single -->
-                </div>
-
-                <!-- profile info-500 -->
-            </div>
             <div class="w-full">
                 <div class="">
-                    <Card title="Info">
+                    <Card title="Información del Hospital">
                         <div class="flex gap-8">
                             <ul class="list space-y-8 w-full">
                                 <li class="flex space-x-3 rtl:space-x-reverse">
                                     <div class="flex-none text-2xl text-slate-600 dark:text-slate-300">
-                                        <Icon icon="heroicons:envelope" />
+                                        <Icon icon="solar:hospital-broken" />
                                     </div>
                                     <div class="flex-1">
                                         <div class="uppercase text-xs text-slate-500 dark:text-slate-300 mb-1 leading-[12px]">
-                                            EMAIL
+                                            Nombre de hospital
                                         </div>
-                                        <a href="mailto:someone@example.com" class="text-base text-slate-600 dark:text-slate-50" v-if="userData">
-                                            {{ userData.email }}
+                                        <a href="mailto:someone@example.com" class="text-base text-slate-600 dark:text-slate-50" v-if="hospitalData">
+                                            {{ hospitalData.name }}
                                         </a>
                                         <p v-else>...</p>
                                     </div>
@@ -84,118 +23,56 @@
                             <!-- end single list -->
                                 <li class="flex space-x-3 rtl:space-x-reverse">
                                     <div class="flex-none text-2xl text-slate-600 dark:text-slate-300">
-                                        <Icon icon="heroicons:phone-arrow-up-right" />
+                                        <Icon icon="ph:dot-outline-fill" />
                                     </div>
                                     <div class="flex-1">
                                         <div class="uppercase text-xs text-slate-500 dark:text-slate-300 mb-1 leading-[12px]">
-                                            CELULAR
+                                            Latitude
                                         </div>
-                                        <a href="tel:0189749676767" class="text-base text-slate-600 dark:text-slate-50" v-if="userData">
-                                        {{ userData.phone_number}}
+                                        <a href="tel:0189749676767" class="text-base text-slate-600 dark:text-slate-50" v-if="hospitalData">
+                                        {{ hospitalData.latitude}}
                                         </a>
-                                    </div>
-                                </li>
-                            <!-- end single list -->
-                                <li class="flex space-x-3 rtl:space-x-reverse">
-                                    <div class="flex-none text-2xl text-slate-600 dark:text-slate-300">
-                                        <Icon icon="cil:birthday-cake" />
-                                    </div>
-                                    <div class="flex-1">
-                                        <div class="uppercase text-xs text-slate-500 dark:text-slate-300 mb-1 leading-[12px]">
-                                            Fecha de nacimiento
-                                        </div>
-                                        <div class="text-base text-slate-600 dark:text-slate-50" v-if="userData">
-                                            {{ userData.birth_date }}
-                                        </div>
                                     </div>
                                 </li>
                             <!-- end single list -->
                             </ul>
-
-
                         <!-- Separacion -->
-
-
-                            <ul class="list space-y-8 w-full">
-                                <li class="flex space-x-3 rtl:space-x-reverse">
-                                    <div class="flex-none text-2xl text-slate-600 dark:text-slate-300">
-                                        <Icon icon="streamline:blood-bag-donation" />
-                                    </div>
-                                    <div class="flex-1">
-                                        <div class="uppercase text-xs text-slate-500 dark:text-slate-300 mb-1 leading-[12px]">
-                                            Nombre
-                                        </div>
-                                        <a href="mailto:someone@example.com" class="text-base text-slate-600 dark:text-slate-50" v-if="userData">
-                                            {{ userData.name }}
-                                        </a>
-                                    </div>
-                                </li>
-                            <!-- end single list -->
-                                <li class="flex space-x-3 rtl:space-x-reverse">
-                                    <div class="flex-none text-2xl text-slate-600 dark:text-slate-300">
-                                        <Icon icon="heroicons:phone-arrow-up-right" />
-                                    </div>
-                                    <div class="flex-1">
-                                        <div class="uppercase text-xs text-slate-500 dark:text-slate-300 mb-1 leading-[12px]">
-                                        Apellidos
-                                        </div>
-                                        <a href="tel:0189749676767" class="text-base text-slate-600 dark:text-slate-50" v-if="userData">
-                                            {{ userData.lastname }}
-                                        </a>
-                                    </div>
-                                </li>
-                            <!-- end single list -->
-                                <li class="flex space-x-3 rtl:space-x-reverse">
-                                    <div class="flex-none text-2xl text-slate-600 dark:text-slate-300">
-                                        <Icon icon="heroicons:map" />
-                                    </div>
-                                    <div class="flex-1">
-                                        <div class="uppercase text-xs text-slate-500 dark:text-slate-300 mb-1 leading-[12px]">
-                                            Alias
-                                        </div>
-                                        <div class="text-base text-slate-600 dark:text-slate-50" v-if="userData">
-                                            {{ userData.alias }}
-                                        </div>
-                                    </div>
-                                </li>
-                            <!-- end single list -->
-                            </ul>
-                            
-
-                        <!-- Separacion -->
-
-
                         <ul class="list space-y-8 w-full">
                                 <li class="flex space-x-3 rtl:space-x-reverse">
                                     <div class="flex-none text-2xl text-slate-600 dark:text-slate-300">
-                                        <Icon icon="streamline:blood-bag-donation" />
+                                        <Icon icon="mdi:address-marker-outline" />
                                     </div>
                                     <div class="flex-1">
                                         <div class="uppercase text-xs text-slate-500 dark:text-slate-300 mb-1 leading-[12px]">
-                                            Tipo de sangre 
+                                            Dirección 
                                         </div>
-                                        <a href="mailto:someone@example.com" class="text-base text-slate-600 dark:text-slate-50" v-if="userData">
-                                            {{ userData.blood_type }}
+                                        <a href="mailto:someone@example.com" class="text-base text-slate-600 dark:text-slate-50" v-if="hospitalData">
+                                            {{ hospitalData.address }}
                                         </a>
                                     </div>
                                 </li>
                             <!-- end single list -->
                                 <li class="flex space-x-3 rtl:space-x-reverse">
                                     <div class="flex-none text-2xl text-slate-600 dark:text-slate-300">
-                                        <Icon icon="heroicons:map" />
+                                        <Icon icon="ph:dot-outline-fill" />
                                     </div>
                                     <div class="flex-1">
                                         <div class="uppercase text-xs text-slate-500 dark:text-slate-300 mb-1 leading-[12px]">
-                                            Sexo
+                                            Longitude
                                         </div>
-                                        <div class="text-base text-slate-600 dark:text-slate-50" v-if="userData">
-                                            {{ userData.sex }}
+                                        <div class="text-base text-slate-600 dark:text-slate-50" v-if="hospitalData">
+                                            {{ hospitalData.longitude }}
                                         </div>
                                     </div>
                                 </li>
                             <!-- end single list -->
                             </ul>
                         </div>
+                        <br>
+                        <router-link 
+                            :to="`/hospitals/${ id }/edit`">
+                            <Button type="submit" text="Crear" btnClass="btn-warning" class="w-1/12">Editar</Button>
+                        </router-link>
                     </Card>
                 </div>
             </div>
@@ -226,20 +103,20 @@
             
             useCachedDataStoreHospitals().fetchData();
 
-            let userData = ref(null); 
+            let hospitalData = ref(null); 
 
             watch(hospitalsTable, () => {
                 console.log(hospitalsTable);
-                userData.value = hospitalsTable.find(objeto => objeto.id == id);
+                hospitalData.value = hospitalsTable.find(objeto => objeto.id == id);
             });
 
             if(hospitalsTable)
-                userData.value = hospitalsTable.find(objeto => objeto.id == id);
+                hospitalData.value = hospitalsTable.find(objeto => objeto.id == id);
 
             return {
                 hospitalsTable,
                 id,
-                userData,
+                hospitalData,
             };
         }
     })
