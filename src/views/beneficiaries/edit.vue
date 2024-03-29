07dc92b98de8528f338528c4d2442b6cedf50718
VictorMRD <victormrd2002@hotmail.com>
2024-03-29 11:44:57 -0700

    Add beneficiaries edit

--- src/views/beneficiaries/edit.vue
+++ src/views/beneficiaries/edit.vue
@@ -2,7 +2,7 @@
     <div class="">
         <div class="p-4 bg-white rounded-md dark:bg-slate-800">
             <div class="flex items-center justify-between py-2">
-                <h3>Crear Beneficiario</h3><br>
+                <h3>Editar Beneficiario</h3><br>
             </div>
             <div class="w-full border-slate-200 border-b-2 dark:border-slate-600"></div>
             <br>
@@ -10,57 +10,101 @@
                 @submit.prevent="onSubmit"
                 class="lg:grid-cols-2 grid gap-5 grid-cols-1"
             >
-                <Textinput
-                    label="Nombre *"
+                <div class="flex gap-0 flex-col justify-center align-middle">
+                    <Textinput
+                        label="Nombre *"
+                        type="text"
+                        placeholder="Ingrese el nombre del beneficiario"
+                        name="name"
+                        v-model="name"
+                        :error="nameError"
+                    />
+                    <p v-if="errors.name" class="mt-2 text-danger-500 block text-sm">{{ errors.name[0] }}</p>
+                </div>
+                <div class="flex gap-0 flex-col justify-center align-middle">
+                    <Textinput
+                    label="Apellidos *"
                     type="text"
-                    placeholder="Ingrese el nombre"
-                    name="name"
-                    v-model="form.name"
-                />
-                <Textinput
-                    label="Apellidos"
-                    type="text"
-                    placeholder="Ingrese sus apellidos"
+                    placeholder="Ingrese los apellidos del beneficiario"
                     name="lastname"
-                    v-model="form.lastname"
-                />
-                <Textinput
-                    label="Fecha de nacimiento"
-                    type="date"
-                    placeholder="Fecha de nacimiento"
-                    name="date"
-                    v-model="form.birth_date"
-                />
-                <Select
-                    label="Tipo de sangre"
-                    type="text"
-                    placeholder="Seleccione su tipo de sangre"
-                    name="bloodtype"
-                    :options="blood_types"
-                    v-model="form.blood_type"
-                />
-                <Textinput
-                    label="CURP"
-                    type="text"
-                    placeholder="Ingrese un curp valido"
-                    name="curp"
-                    v-model="form.curp"
-                />
+                    v-model="lastname"
+                    :error="lastnameError"
+                    />
+                    <p v-if="errors.lastname" class="mt-2 text-danger-500 block text-sm">{{ errors.lastname[0] }}</p>
+                </div>
+                <div class="flex gap-0 flex-col justify-center align-middle">
+                    <Textinput
+                        label="Fecha de nacimiento *"
+                        type="date" 
+                        placeholder="Ingrese la fecha de nacimiento del beneficiario"
+                        name="birth_date"
+                        v-model="birth_date"
+                        :error="birth_dateError"
+                    />
+                    <p v-if="errors.birth_date" class="mt-2 text-danger-500 block text-sm">{{ errors.birth_date[0] }}</p>
+                </div>
+
+                <div class="flex gap-0 flex-col justify-center align-middle">
+                    <Textinput
+                        label="curp *"
+                        type="text"
+                        placeholder="Ingrese el curp del beneficiario"
+                        name="curp"
+                        v-model="curp"
+                        :error="curpError"
+                    />
+                    <p v-if="errors.curp" class="mt-2 text-danger-500 block text-sm">{{ errors.curp[0] }}</p>
+                </div>
+                <div class="flex gap-0 flex-col justify-center align-middle">
+                    <Select
+                        label="Tipo de sangre"
+                        type="text"
+                        placeholder="Seleccione su tipo de sangre"
+                        name="bloodtype"
+                        :options="blood_types"
+                        v-model="blood_type"
+                        :error="blood_typeError"
+                    />
+                    <p v-if="errors.blood_type" class="mt-2 text-danger-500 block text-sm">{{ errors.blood_type[0] }}</p>
+                </div>
                 <div class="lg:col-span-2 gap-2 flex">
-                    <Button type="button" text="Crear" btnClass="btn-primary" @click="displayConfirmMessage()"></Button>
+                    <Button type="submit" text="Editar" btnClass="btn-primary"></Button>
                     <router-link
                         :to="{ path:  '/beneficiaries/' }"
                     ><Button btnClass="btn-dark" text="Cancelar" /></router-link>
                 </div>
             </form>
         </div>
-        <div class="absolute w-1/4 shadow-xl top-1/3 right-1/3" v-if="confirmMessage">
+        <div class="absolute w-1/4 shadow-xl top-1/3 right-1/3" v-if="confirmMessageFlag">
             <Card title="Se requiere confirmación" class="text-center" noborder>
                 Estas a punto de agregar una nueva entidad a la base de datos.<br>
                 ¿Estás seguro que quieres continuar?
+                <br><br>
+                <div>
+                    <div>
+                        <p class="font-bold">Nombre del Beneficiario:</p>
+                        {{ name }}
+                    </div>
+                    <div>
+                        <p class="font-bold">Apellido:</p>
+                        {{ lastname }}
+                    </div>
+                    <div>
+                        <p class="font-bold">Fecha de nacimiento:</p>
+                        {{ birth_date }}
+                    </div>
+                    <div>
+                        <p class="font-bold">Curp:</p>
+                        {{ curp }}
+                    </div>
+                    <div>
+                        <p class="font-bold">Tipo de sangre:</p>
+                        {{ blood_type }}
+                    </div>
+                </div>
                 <div class="mt-9 flex justify-evenly">
-                    <Button btnClass="btn-primary" text="Confirmar" @click="createUser()" />
-                    <Button btnClass="btn-dark" text="Cancelar" @click="displayConfirmMessage()" />
+                    <Button btnClass="btn-primary" text="Confirmar" @click="editBeneficiary()" />
+                    <Button btnClass="btn-dark" text="Retroceder" @click="displayConfirmMessage()" />
                 </div>
             </Card>
         </div>
@@ -74,9 +118,13 @@
     import Textinput from "@/components/Textinput";
     import { useField, useForm } from "vee-validate";
     import Select from "@/components/Select";
-    import { useCachedDataStoreBeneficiaries } from '@/stores/beneficiariesStore';
-    import { useRouter } from 'vue-router';
     import { ref, watch } from 'vue';
+    import axios from "@/plugins/axios";
+    import * as yup from 'yup';
+    import { useCachedDataStoreBeneficiaries } from '../../stores/beneficiariesStore';
+    import { useToast } from "vue-toastification";
+    import router from '../../router';
+    import { useRouter } from 'vue-router';
 
     export default {
         components: {
@@ -84,24 +132,95 @@
             Button,
             Textarea,
             Select,
+            Textinput,
             Card
         },
-        props: {
-            formInformation: Object,
-        },
         setup() {
-            
+            const schema = yup.object().shape({
+                name: yup.string()
+                    .required("El nombre del beneficiario es requerido")
+                    .min(3, "El nombre debe de contener al menos 3 caracteres")
+                    .matches(/^[^\d]+$/, "El nombre no puede contener numeros"),
+                lastname: yup.string()
+                    .required("Los apellidos son requeridos")
+                    .min(3, "El apellido debe de contener al menos 3 caracteres")
+                    .matches(/^[^\d]+$/, "El apellido no puede contener numeros"),
+                birth_date: yup.string()
+                    .required("La fecha de nacimiento es requerida"),
+                curp: yup.string()
+                    .required("El curp es requerido")
+                    .max(18, "El curp no puede exceder los 18 caracteres")
+                    .min(18, "El curp debe de tener al menos 18 caracteres"),
+                blood_type: yup.string()
+                    .required("El tipo de sangre es requerido")
+            });
+
+            let errors = ref([]);
+            let errorMessage = ref("");
+            let formValues = ref([]);
+            let confirmMessageFlag = ref(false);
+            const toast = useToast();
 
             const { handleSubmit } = useForm({
-            });
-            // No need to define rules for fields
-
-            const onSubmit = handleSubmit(() => {
-                // console.warn(values.email);
+                validationSchema: schema,
             });
 
+            const trySubmit = handleSubmit(async (values) => {
+                formValues = values;
+                displayConfirmMessage();
+            }); 
 
-            /* No de la template */
+            const onSubmit = handleSubmit((values) => {
+                const newBeneficiaryForm = [
+                    { name: 'name', value: name.value },
+                    { name: 'lastname', value: lastname.value },
+                    { name: 'birth_date', value: birth_date.value },
+                    { name: 'curp', value: curp.value },
+                    { name: 'blood_type', value: blood_type.value },
+                ];
+                trySubmit(newBeneficiaryForm);
+            });
+            
+            function displayConfirmMessage(){
+                confirmMessageFlag.value = !confirmMessageFlag.value;
+            }
+            
+            function userRedirect(){
+                router.push('/beneficiaries', {shallow: false});
+            }
+
+            function editBeneficiary(){
+                confirmMessageFlag.value = false;
+                axios.put(`/api/beneficiaries/${beneficiaryId}`, formValues)
+                .then(res => {
+                    console.log(res);
+                    useCachedDataStoreBeneficiaries().refreshData();
+                    toast.success("¡Beneficiario creado correctamente!", { timeout: 1000 });
+                    setTimeout(userRedirect, 1000);
+                })
+                .catch(error => {
+                    toast.error("Ha ocurrido un error inesperado.", { timeout: 2000 });
+                    if (error.response && error.response.data) {
+                        const responseData = error.response.data;
+                        errorMessage.value = responseData.message || 'An error occurred.';
+
+                        if (responseData.errors) {
+                            errors.value = responseData.errors;
+                        }
+                    } else {
+                        errorMessage.value = 'An error occurred.';
+                    }
+                    console.log(errors)
+                    console.log(errorMessage)
+                });
+            }
+
+            const { value: name, errorMessage: nameError } = useField("name");
+            const { value: lastname, errorMessage: lastnameError } = useField("lastname");
+            const { value: birth_date, errorMessage: birth_dateError } = useField("birth_date");
+            const { value: curp, errorMessage: curpError } = useField("curp");
+            const { value: blood_type, errorMessage: blood_typeError } = useField("blood_type");
+
             const blood_types = [
                 { value: 'A+', label: 'A+' },
                 { value: 'A-', label: 'A-' },
@@ -112,78 +231,52 @@
                 { value: 'O+', label: 'O+' },
                 { value: 'O-', label: 'O-' }
             ];
-            const rolTypes = [
-                { value: "administrator", label: "administrator" },
-                { value: "agent", label: "agent" },
-                { value: "participant", label: "participant" },
-            ];
-
-            /* Not from the template */
 
             const router = useRouter();
             const { beneficiariesTable } = useCachedDataStoreBeneficiaries();
-            const id = router.currentRoute.value.params.id;
-            
+            const beneficiaryId = router.currentRoute.value.params.id;
             useCachedDataStoreBeneficiaries().fetchData();
-
-            let userData = ref(null); 
-            let form = ref({
-                name: null,
-                lastname: null,
-                alias: null,
-                birth_date: null,
-                blood_type: null,
-                phone_number: null,
-                curp: null,
-                email: null,
-                password: null,
-                role: null,
-            })
-
+            function passBeneficiaryValuesToSingleVariables() {
+                name.value = beneficiarieData.value.name;
+                lastname.value = beneficiarieData.value.lastname;
+                birth_date.value = beneficiarieData.value.birth_date;
+                curp.value = beneficiarieData.value.curp;
+                blood_type.value = beneficiarieData.value.blood_type;
+            }
+            let beneficiarieData = ref(null); 
             watch(beneficiariesTable, () => {
-                userData.value = beneficiariesTable.find(objeto => objeto.id == id);
-                assignFormValues();
-            });
-
-            if(beneficiariesTable){
-                userData.value = beneficiariesTable.find(objeto => objeto.id == id);
-                assignFormValues();
-            }
-
-            function assignFormValues(){
-                if(userData.value){
-                    form.value.name = userData.value.name;
-                    form.value.lastname = userData.value.lastname;
-                    form.value.alias = userData.value.alias;
-                    form.value.birth_date = userData.value.birth_date;
-                    form.value.blood_type = userData.value.blood_type;
-                    form.value.phone_number = userData.value.phone_number;
-                    form.value.curp = userData.value.curp;
-                    form.value.email = userData.value.email;
-                    form.value.role = userData.value.role;
+                beneficiarieData.value = beneficiariesTable.find(objeto => objeto.id == beneficiaryId);
+                if(beneficiarieData.value != null){
+                    console.log(beneficiarieData.value);
+                    passBeneficiaryValuesToSingleVariables();
                 }
-            }
-
-            let confirmMessage = ref(false)
-            function displayConfirmMessage(){
-                console.log(confirmMessage.value);
-                confirmMessage.value = !confirmMessage.value;
-            }
-            function editUser(){
-                confirmMessage.value = false;
-                console.log("Usuario editado");
+            });
+            if(beneficiariesTable){
+                beneficiarieData.value = beneficiariesTable.find(objeto => objeto.id == beneficiaryId);
+                if(beneficiarieData.value != null){
+                    console.log(beneficiarieData.value);
+                    passBeneficiaryValuesToSingleVariables();
+                }
             }
 
             return {
                 blood_types,
-                rolTypes,
-                onSubmit,
-                beneficiariesTable,
-                userData,
-                form,
-                confirmMessage,
+                editBeneficiary,
                 displayConfirmMessage,
-                editUser
+                confirmMessageFlag,
+                name,
+                nameError,
+                lastname,
+                lastnameError,
+                birth_date,
+                birth_dateError,
+                curp,
+                curpError,
+                blood_type,
+                blood_typeError,
+                onSubmit,
+                errors,
+                errorMessage
             };
         }
     }
