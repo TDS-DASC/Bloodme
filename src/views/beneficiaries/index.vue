a88311f4d5d2ef2824175a7ab15a79485316b2c4
VictorMRD <victormrd2002@hotmail.com>
2024-03-29 11:45:25 -0700

    Add create and refresh button to beneficiaries

--- src/views/beneficiaries/index.vue
+++ src/views/beneficiaries/index.vue
@@ -1,17 +1,30 @@
 <template>
     <div class="flex flex-col gap-4">
         <tableAdvanced :tableInformation=beneficiariesTableParams :tableData=beneficiariesTable />
+        <div class="flex gap-3 w-1/4">
+            <router-link 
+                to="/beneficiaries/create" class="w-full">
+                <Button type="submit" text="Crear" btnClass="btn-primary" class="w-full">Crear</Button>
+            </router-link>
+            <router-link :to="{ path: '/refresh', query: { urlHeader: 'beneficiaries' } }" class="w-full">
+                <Button type="submit" text="Refrescar" btnClass="btn-secondary" class="w-full" @click="refreshToast()">Refrescar</Button>
+            </router-link>
+        </div>
     </div>
 </template>
 
 <script>
+    import Button from "@/components/Button";
     import tableAdvanced from "../../components/Table/advanced"
     import { useCachedDataStoreBeneficiaries } from '../../stores/beneficiariesStore';
+    import { useToast } from "vue-toastification";
     export default{
         components: {
-            tableAdvanced
+            tableAdvanced,
+            Button
         },
         setup() {
+            const toast = useToast();
             const beneficiariesTableParams = {
                 title: "Agentes",
                 rows: 5,
@@ -42,8 +55,12 @@
             const { beneficiariesTable } = useCachedDataStoreBeneficiaries();
             useCachedDataStoreBeneficiaries().fetchData();
 
+            function refreshToast(){
+                toast.warning("Refrescando la tabla.", { timeout: 1000 });
+            }
 
             return {
+                refreshToast,
                 beneficiariesTableParams,
                 beneficiariesTable,
             }
