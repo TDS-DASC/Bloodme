8368df64596f1c8efba09d882a6d6cd41324847a
VictorMRD <victormrd2002@hotmail.com>
2024-03-31 14:12:40 -0700

    Add buttons to campaigns index

--- src/views/campaigns/index.vue
+++ src/views/campaigns/index.vue
@@ -1,18 +1,32 @@
 <template>
     <div class="flex flex-col gap-4 p-2">
-        <tableAdvanced :tableInformation=tableParams />
+        <tableAdvanced :tableInformation=campaignsTableParams :tableData=campaignsTable urlMainHeader="hospitals" />
+        <div class="flex gap-3 w-1/4">
+            <router-link 
+                to="/campaigns/create" class="w-full">
+                <Button type="submit" text="Crear" btnClass="btn-primary" class="w-full">Crear</Button>
+            </router-link>
+            <router-link :to="{ path: '/refresh', query: { urlHeader: 'campaigns' } }" class="w-full">
+                <Button type="submit" text="Refrescar" btnClass="btn-secondary" class="w-full" @click="refreshToast()">Refrescar</Button>
+            </router-link>
+        </div>
     </div>
 </template>
 
 <script>
-    import tableAdvanced from "../../components/table/advanced/Advanced.vue"
+    import Button from "@/components/Button";   
+    import tableAdvanced from "../../components/Table/advanced"
+    import { useCachedDataStoreCampaigns } from '../../stores/campaignsStore';
+    import { useToast } from "vue-toastification";
 
     export default{
         components: {
-            tableAdvanced
+            tableAdvanced,
+            Button
         },
         setup() {
-            const tableParams = {
+            const toast = useToast();
+            const campaignsTableParams = {
                 title: "Campañas",
                 rows: 5,
                 urls: [
@@ -24,16 +38,16 @@
                         field: "id",
                     },
                     {
-                        label: "Nombre",
-                        field: "customer",
+                        label: "Bolsas recolectadas",
+                        field: "recollected_bags",
                     },
                     {
-                        label: "Direccion",
-                        field: "quantity",
+                        label: "Blosas requeridas",
+                        field: "required_bags",
                     },
                     {
-                        label: "Hora de atención",
-                        field: "text",
+                        label: "Plaquetas requeridas",
+                        field: "required_platelets",
                     },
                     {
                         label: "action",
@@ -42,8 +56,21 @@
                 ]
             }
 
+            let { campaignsTable } = useCachedDataStoreCampaigns();
+            useCachedDataStoreCampaigns().fetchData();
+
+            function refreshToast(){
+                toast.warning("Refrescando la tabla.", { timeout: 1000 });
+            }
+
             return {
-                tableParams,
+                refreshToast,
+                campaignsTableParams,
+                campaignsTable,
+            }
+
+            return {
+                campaignsTableParams,
             }
         },
     }
