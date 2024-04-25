51f084c81599422c91a9f434425b7c26d670fc4b
VictorMRD <116592163+VictorMRD@users.noreply.github.com>
2024-04-24 21:18:07 -0700

    Add loading icon to modules index

--- src/views/appointments/index.vue
+++ src/views/appointments/index.vue
@@ -1,6 +1,19 @@
 <template>
     <div class="flex flex-col gap-4">
         <tableAdvanced :tableInformation=appointmentsTableParams :tableData=appointmentsTable urlMainHeader="appointments"  />
+        <div v-if="loadingFlag" class="fixed inset-0 flex flex-col justify-center items-center bg-white bg-opacity-0 z-10">
+            <div class="bg-white flex flex-col justify-center align-middle items-center p-10 rounded-md border-2 border-gray-300">
+                <p class="font-bold text-4xl mb-4">Cargando</p>
+                <div class="w-24 h-24">
+                    <svg xmlns="http://www.w3.org/2000/svg" width="100%" height="100%" viewBox="0 0 24 24">
+                        <path fill="none" stroke="currentColor" stroke-dasharray="15" stroke-dashoffset="15" stroke-linecap="round" stroke-width="2" d="M12 3C16.9706 3 21 7.02944 21 12">
+                        <animate fill="freeze" attributeName="stroke-dashoffset" dur="0.3s" values="15;0"/>
+                        <animateTransform attributeName="transform" dur="1.5s" repeatCount="indefinite" type="rotate" values="0 12 12;360 12 12"/>
+                        </path>
+                    </svg>
+                </div>
+            </div>
+        </div>
     </div>
 </template>
 
@@ -11,6 +24,7 @@
     import { useCachedDataStoreCampaigns } from '../../stores/campaignsStore';
     import { useCachedDataStoreBeneficiaries } from '../../stores/beneficiariesStore';
     import { useToast } from "vue-toastification";
+    import { ref } from "vue";
 
     export default{
         components: {
@@ -49,6 +63,8 @@
                     },
                 ]
             }
+
+            let loadingFlag = ref(true);
             
             const { appointmentsTable} = useCachedDataStoreAppointments();
             const { participantsTable} = useCachedDataStoreParticipants();
@@ -74,6 +90,7 @@
                         appointment.beneficiary = beneficiaryData.find(beneficiary => beneficiary.id == campaign.beneficiary).name;
                     }
                 });
+                loadingFlag.value = false;
             }
             fetchData();
 
@@ -85,6 +102,7 @@
                 refreshToast,
                 appointmentsTableParams,
                 appointmentsTable,
+                loadingFlag
             }
         },
     }
