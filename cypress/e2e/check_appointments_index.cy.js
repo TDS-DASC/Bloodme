4ace4e9c515ca07128e3d108f4148f974023fa91
VictorMRD <victormrd2002@hotmail.com>
2024-08-21 09:11:16 -0700

    Cypress test for appointment index

---
+++ cypress/e2e/check_appointments_index.cy.js
@@ -0,0 +1,32 @@
+describe('Load appointments index', () => {
+    it('successfully logs in and load index', () => {
+        // Visit the login page
+        cy.visit('http://localhost.local:5173');
+
+        // Perform the request to get the CSRF cookie
+        cy.request('http://localhost.local:8000/sanctum/csrf-cookie').then((response) => {
+            expect(response.status).to.eq(204);
+
+            // Extract the CSRF token from cookies
+            const csrfToken = cy.getCookie('XSRF-TOKEN');
+            const sessionToken = cy.getCookie('laravel_session');
+
+            // Perform login
+            cy.get('input[type=email]').type('admin@admin.com').should('have.value', 'admin@admin.com');
+            cy.get('input[type=password]').type('admin').should('have.value', 'admin');
+            cy.contains('Iniciar Sesion').click();
+
+            // Save user info in localStorage
+            cy.window().then((win) => {
+                win.localStorage.setItem('user', JSON.stringify({ email: 'admin@admin.com', password: 'admin' }));
+            });
+        });
+
+        cy.location('pathname', {timeout: 8000})
+            .should('include', '/dashboard')
+        cy.window().then((win) => {
+            console.log(win.location)
+            })
+        cy.visit('http://localhost.local:5173/appointments/index');
+    });
+});
