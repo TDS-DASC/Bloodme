1a2f3d2ce9a0792437e18c4b780504669467cdf5
VictorMRD <victormrd2002@hotmail.com>
2025-02-06 16:54:49 -0700

    Standarize view screen of participants module

--- src/views/participants/view.vue
+++ src/views/participants/view.vue
@@ -1,10 +1,11 @@
 <template>
     <div class="">
         <div class="space-y-5 profile-page">
-            <div class="profiel-wrap px-[35px] pb-10 md:pt-[84px] pt-10 rounded-lg bg-white dark:bg-slate-800 lg:flex lg:space-y-0 space-y-6 justify-between items-end relative z-[1]">
-                <div class="bg-slate-900 dark:bg-slate-700 absolute left-0 top-0 md:h-1/2 h-[150px] w-full z-[-1] rounded-t-lg"></div>    
-                    <div class="profile-box flex-none md:text-start text-center">
-                        <div class="md:flex items-end md:space-x-6 rtl:space-x-reverse">
+            <div class="w-full">
+                <div class="">
+                    <Card title="Info">
+                        
+                        <div class="flex gap-8">
                             <div class="flex-none">
                                 <div class="md:h-[100px] md:w-[100px] h-[140px] w-[140px] md:ml-0 md:mr-0 ml-auto mr-auto md:mb-0 mb-4 rounded-full ring-4 ring-slate-100 relative">
                                     <img :src="profileImg" alt="" class="w-full h-full object-cover rounded-full" v-if="userData" />
@@ -17,56 +18,6 @@
                                         </router-link>
                                 </div>
                             </div>
-                            <div class="flex-1">
-                                <div class="text-2xl font-medium text-slate-900 dark:text-slate-200 mb-[3px]" v-if="userData">
-                                    {{ userData.name }} {{ userData.lastname }}
-                                </div>
-                                <div class="text-2xl font-medium text-slate-900 dark:text-slate-200 mb-[3px]" v-else>
-                                    Cargando...
-                                </div>
-                                <div class="text-sm font-light text-slate-600 dark:text-slate-400">
-                                    Nombre completo
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                    <!-- end profile box -->
-                    <div    class="profile-info-500 md:flex md:text-start text-center flex-1 max-w-[516px] md:space-y-0 space-y-4">
-                        <div class="flex-1">
-                            <div class="text-base text-slate-900 dark:text-slate-300 font-medium mb-1">
-                                33
-                            </div>
-                            <div class="text-sm text-slate-600 font-light dark:text-slate-300">
-                                Donaciones totales
-                            </div>
-                        </div>
-                    <!-- end single -->
-                    <div class="flex-1">
-                        <div class="text-base text-slate-900 dark:text-slate-300 font-medium mb-1">
-                            2
-                        </div>
-                        <div class="text-sm text-slate-600 font-light dark:text-slate-300">
-                            Campañas creadas
-                        </div>
-                    </div>
-                    <!-- end single -->
-                    <div class="flex-1">
-                        <div class="text-base text-slate-900 dark:text-slate-300 font-medium mb-1">
-                            9 
-                        </div>
-                        <div class="text-sm text-slate-600 font-light dark:text-slate-300">
-                            Citas agendadas
-                        </div>
-                    </div>
-                    <!-- end single -->
-                </div>
-
-                <!-- profile info-500 -->
-            </div>
-            <div class="w-full">
-                <div class="">
-                    <Card title="Info">
-                        <div class="flex gap-8">
                             <ul class="list space-y-8 w-full">
                                 <li class="flex space-x-3 rtl:space-x-reverse">
                                     <div class="flex-none text-2xl text-slate-600 dark:text-slate-300">
@@ -268,6 +219,7 @@
             watch(participantsTable, () => {
                 console.log(participantsTable);
                 userData.value = participantsTable.find(objeto => objeto.id == id);
+                console.log(userData.value);
             });
 
             if(participantsTable)
