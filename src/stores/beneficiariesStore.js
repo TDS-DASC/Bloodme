ced574e11393670c8de542ff3e70485860b9f336
VictorMRD <victormrd2002@hotmail.com>
2025-02-06 13:54:34 -0700

    Changes to beneficiaries pinia store

--- src/stores/beneficiariesStore.js
+++ src/stores/beneficiariesStore.js
@@ -16,7 +16,7 @@ export const useCachedDataStoreBeneficiaries = defineStore({
         try {
           const response = await axios.get(`/api/beneficiaries`);
           response.data.forEach(beneficiary => {
-            if(this.beneficiariesTable.length <= response.data.length)
+            if(this.beneficiariesTable.length <= response.data.length-1)
               this.beneficiariesTable.push(beneficiary);
           });
           this.dataLoaded = true;
