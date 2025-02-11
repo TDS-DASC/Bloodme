044387a7adc5c3f6571c1e546c05379dc36e6d72
VictorMRD <victormrd2002@hotmail.com>
2025-02-11 11:48:11 -0700

    Standarize administrator view screen

--- src/views/administrators/view.vue
+++ src/views/administrators/view.vue
@@ -1,60 +1,15 @@
 <template>
     <div class="">
         <div class="space-y-5 profile-page">
-            <div class="profiel-wrap px-[35px] pb-10 md:pt-[84px] pt-10 rounded-lg bg-white dark:bg-slate-800 lg:flex lg:space-y-0 space-y-6 justify-between items-end relative z-[1]">
-                <div class="bg-slate-900 dark:bg-slate-700 absolute left-0 top-0 md:h-1/2 h-[150px] w-full z-[-1] rounded-t-lg"></div>    
-                    <div class="profile-box flex-none md:text-start text-center">
-                        <div class="md:flex items-end md:space-x-6 rtl:space-x-reverse  h-[140px]">
-                            <div class="flex-1">
-                                <div class="text-2xl font-medium text-slate-900 dark:text-slate-200 mb-[3px]" v-if="userData">
-                                    {{ userData.name }} {{ userData.lastname }}
-                                </div>
-                                <div class="text-2xl font-medium text-slate-900 dark:text-slate-200 mb-[3px]" v-else>
-                                    Cargando...
-                                </div>
-                                <div class="text-sm font-light text-slate-600 dark:text-slate-400">
-                                    Nombre completo
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                    <!-- end profile box -->
-                    <!-- <div    class="profile-info-500 md:flex md:text-start text-center flex-1 max-w-[516px] md:space-y-0 space-y-4">
-                        <div class="flex-1">
-                            <div class="text-base text-slate-900 dark:text-slate-300 font-medium mb-1">
-                                33
-                            </div>
-                            <div class="text-sm text-slate-600 font-light dark:text-slate-300">
-                                Donaciones totales
-                            </div>
-                        </div>
-                        <div class="flex-1">
-                            <div class="text-base text-slate-900 dark:text-slate-300 font-medium mb-1">
-                                2
-                            </div>
-                            <div class="text-sm text-slate-600 font-light dark:text-slate-300">
-                                Campañas creadas
-                            </div>
-                        </div>
-                        <div class="flex-1">
-                            <div class="text-base text-slate-900 dark:text-slate-300 font-medium mb-1">
-                                9 
-                            </div>
-                            <div class="text-sm text-slate-600 font-light dark:text-slate-300">
-                                Citas agendadas
-                            </div>
-                        </div>
-                    </div> -->
-            </div>
+            
             <div class="w-full">
                 <div class="">
                     <Card title="Info">
                         <div class="flex gap-8">
-                        <!-- Separacion -->
                             <ul class="w-full flex items-center align-middle gap-20">
                                 <li class="flex space-x-3 rtl:space-x-reverse">
                                     <div class="flex-none text-2xl text-slate-600 dark:text-slate-300">
-                                        <Icon icon="wpf:name" />
+                                        <Icon icon="line-md:account" />
                                     </div>
                                     <div class="flex-1">
                                         <div class="uppercase text-xs text-slate-500 dark:text-slate-300 mb-1 leading-[12px]">
